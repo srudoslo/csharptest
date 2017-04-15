14576f89a6eb641b8ca9b0f14e98e169db753a79
srudoslo <kaimasy@yahoo.com>
2017-04-16 00:18:46 +0200

    first csharp program
    This is my first variable in c sharp

---
+++ First variables/First variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("my variable value ={0}",x);
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
