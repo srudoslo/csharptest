9a48e2d0e862fd1252159eb3bd750b6082b87779
srudoslo <kaimasy@yahoo.com>
2017-04-17 21:50:56 +0200

    Literal characters
    my corse about literal characters

---
+++ literal character/literal character/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace literal_character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar     = -100;
+            uint myuVar   = 1000u;
+            float myflvar = 1.5f;
+            double mydvar = 100.5;
+            decimal mydecvar = 100.5m;
+            Console.WriteLine("{0}{1}", myVar, myuVar);
+            Console.WriteLine($"{mydecvar}{mydvar}");
+            Console.ReadKey();
+        }
+    }
+}
